acb0795327f5a568552e638c82e77c88f5ecb921
Tifenn Guillas <tifenn.guillas@gmail.com>
2022-01-22 15:47:21 +0100

    Form validation => DONE

---
+++ src/app/person/person-generator/gender-selection-validator.directive.spec.ts
@@ -0,0 +1,23 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { GenderSelectionValidator } from './gender-selection-validator.directive';
+
+describe('GenderSelectionValidator', () => {
+	it('should return valid', () => {
+		let form: FormGroup = new FormGroup({
+			male: new FormControl(true),
+			female: new FormControl(true)
+		}, { validators: GenderSelectionValidator });
+		expect(form.errors).toBeNull();
+
+		form.controls.male.setValue(false);
+		expect(form.errors).toBeNull();
+	});
+
+	it('should return noGenderSelected error', () => {
+		const form: FormGroup = new FormGroup({
+			male: new FormControl(false),
+			female: new FormControl(false)
+		}, { validators: GenderSelectionValidator });
+		expect(form.errors).toBeTruthy();
+	});
+});
--- src/app/person/person-generator/person-generator.component.ts
+++ src/app/person/person-generator/person-generator.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 import { FormBuilder, FormGroup, FormControl, ValidationErrors, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { GenerationConfig } from "../generation-config";
+import { GenderSelectionValidator } from './gender-selection-validator.directive';
 
 @Component({
 	selector: "app-person-generator",
@@ -12,13 +13,26 @@ export class PersonGeneratorComponent {
 	@Output() generateRequest = new EventEmitter<GenerationConfig>();
 
 	generator: FormGroup = new FormGroup({
-		count: new FormControl(1000),
+		count: new FormControl(1000, {
+			validators: [
+				Validators.required,
+				Validators.min(1),
+				Validators.max(1000)
+			],
+			updateOn: 'change'
+		}),
 		male: new FormControl(true),
 		female: new FormControl(true)
-	});
+	}, { validators: GenderSelectionValidator });
 
 
 	constructor() { }
+
+	log() {
+		console.log(this.generator.valid);
+		console.log(this.generator.value);
+		console.log(this.generator.hasError('noGenderSelected'));
+	}
 
 	generate(): void {
 		const value: GenerationConfig = this.generator.value;
